198a2ceddf9ca5a8ce43c733a93af210b9ba6465
prui <prui_0220@163.com>
2016-10-19 13:39:59 +0800

    update bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
+var concat = require('gulp-concat');
 
 
 var path = {
@@ -11,6 +12,7 @@ var path = {
 
 gulp.task('scripts', function() {
     return gulp.src(path.scripts)
+        .pipe(concat('id-validator.js'))
         .pipe(uglify())
         .pipe(rename({
             extname: '.min.js'
